7cb5fbab61a398afd23f271062048facd1b5e31f
Victor Escandiel <joaoescandielb@gmail.com>
2023-10-11 17:32:27 -0300

    Initializing the project with Spring Boot and Maven

---
+++ src/main/java/santanderdio/javaspringboot/JavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package santanderdio.javaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootApplication.class, args);
+	}
+
+}
