325344a9e03dc210f065054db59040f38fd2feff
Victor Escandiel <joaoescandielb@gmail.com>
2023-10-11 17:39:27 -0300

    new class: MyApp;

---
+++ src/main/java/santanderdio/javaspringboot/MyApp.java
@@ -0,0 +1,12 @@
+package santanderdio.javaspringboot;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyApp implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
